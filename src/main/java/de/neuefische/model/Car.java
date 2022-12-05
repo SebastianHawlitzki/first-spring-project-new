1bd574c032047650cdbd4c004ea888c029c5e4a1
sebastianhawlitzki <business.hawlitzki@gmail.com>
2022-12-05 16:49:55 +0100

    correction

--- src/main/java/de/neuefische/model/Car.java
+++ src/main/java/de/neuefische/model/Car.java
@@ -2,9 +2,11 @@ package de.neuefische.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Car {
 
     private String manufacturer;
